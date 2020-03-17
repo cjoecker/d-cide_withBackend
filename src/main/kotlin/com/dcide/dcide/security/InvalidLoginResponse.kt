17a82d963774ef650eba829caa8df25a35a4df70
ChristianOffice <christian.joecker@maibornwolff.de>
2020-03-17 21:27:39 +0100

    Repaired login wrong password

--- src/main/kotlin/com/dcide/dcide/security/InvalidLoginResponse.kt
+++ src/main/kotlin/com/dcide/dcide/security/InvalidLoginResponse.kt
@@ -1,5 +1,5 @@
 package com.dcide.dcide.security
 
 data class InvalidLoginResponse(
-        val error: String =  "Invalid username or password"
+        val password: String =  "Invalid username or password"
 )
